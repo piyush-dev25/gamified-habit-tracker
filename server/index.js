2da885fecc4d61c7462e4a9470d75bd49b64d85e
Piyush <piyush.25june@gmail.com>
2025-12-25 12:18:08 +0530

    feat:changed index.js for render

--- server/index.js
+++ server/index.js
@@ -12,9 +12,16 @@ dotenv.config();
 connectDB();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
